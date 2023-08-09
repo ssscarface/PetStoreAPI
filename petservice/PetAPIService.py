058fb2fdd9afd1327599e5fc954fd0ebffdd36e1
Your Name <you@example.com>
2023-08-09 13:15:20 +0300

    Fixed

--- petservice/PetAPIService.py
+++ petservice/PetAPIService.py
@@ -15,7 +15,7 @@ class PetAPIService:
 
     def get_pet_by_id(self, pet_id):
         response = APIClient(pestStoreUrl).get(f"pet/{pet_id}")
-        return ApiResponse(response)
+        return ApiResponse(response).convert_response_to_pet_model()
 
     def post_pet_by_id(self,payload, pet_id):
         response = APIClient(pestStoreUrl).post_by_ID(f"pet/{pet_id}", payload)
